1ee26ec2bcc532733f36ca1944d861a13d074216
Khazraie Shaneivar Shervin <khazrai1@cdf.toronto.edu>
2017-01-27 17:15:51 -0500

    removed follower based functions

--- utils.c
+++ utils.c
@@ -37,34 +37,4 @@ int print_dat_file(char *filename) {
 	return 0;
 }
 
-/** return index of follower in a list of followers, return -1 if it does not exist **/
-int exists(int followers[], int unique, int follower){
-	int i;
-	for (i=0; i < unique * 2; i=i+2){
-		if(followers[i] == follower){
-			return i;
-		}
-	}
-	return -1;
-}
 
-/** print out the max and average for followers **/
-void print_max_avg(int followers[], int unique){
-	/* print max and average */
-	int i;
-	int max = 0;
-	float total = 0;
-	for (i=0; i < unique * 2; i=i+2){
-		if (followers[i+1] > max) max = followers[i+1];
-		total += followers[i+1]; 
-	}
-	printf("Max: %d  Average: %.3f\n", max, total/unique);
-}
-
-/** print out each unique follower and how many people they are following, testing only **/ 
-void print_followers(int followers[], int unique){
-	int i;
-	for (i=0; i < unique * 2; i=i+2){
-		printf("follower: %d following: %d\n", followers[i], followers[i+1]);
-	}
-}
