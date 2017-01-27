d1e4cb2e75049004fdb4f25071ac7e7bd431180e
Khazraie Shaneivar Shervin <khazrai1@cdf.toronto.edu>
2017-01-27 17:17:47 -0500

    removed headers

--- utils.h
+++ utils.h
@@ -9,11 +9,4 @@ void print_buffer(Record r[]);
 /** Print out the content of a binary file **/
 int print_dat_file(char *filename);
 
-/** print out each unique follower and how many people they are following, testing only **/
-void print_followers(int followers[], int unique);
 
-/** print out the max and average for followers **/
-void print_max_avg(int followers[], int unique);
-
-/** return index of follower in a list of followers, return -1 if it does not exist **/
-int exists(int followers[], int unique, int follower);
