ac10e977e9ed798f0503ba56fdbfc6ad58b62218
Ashiq <ashiq_silent@yahoo.com>
2017-03-02 02:28:52 -0500

    removed print dat from makefile

--- A1-2/graph.py
+++ A1-2/graph.py
@@ -2,36 +2,36 @@ import matplotlib.pyplot as plt
 import math
 import xlsxwriter
 
-f1 = open("result.txt", "r")
-f2 = open("result2.txt", "r")
-x1 = []
-y1 = []
-x1_real = []
-y1_real = []
-for line in f1.readlines():
-    l = line.strip().split(',')
-    x1.append(math.log(int(l[0])))
-    x1_real.append(int(l[0]))
-    if int(l[1]) > 0:
-        y1.append(math.log(int(l[1])))
-        y1_real.append(int(l[1]))
-    else:
-        y1.append(0.0)
-        y1_real.append(0)
-x2 = []
-y2 = []
-x2_real = []
-y2_real = []
-for line in f2.readlines():
-    l = line.strip().split(',')
-    x2.append(math.log(int(l[0])))
-    x2_real.append(int(l[0]))
-    if int(l[1]) > 0:
-        y2.append(math.log(int(l[1])))
-        y2_real.append(int(l[1]))
-    else:
-        y2.append(0.0)
-        y2_real.append(0)
+#f1 = open("result.txt", "r")
+#f2 = open("result2.txt", "r")
+#x1 = []
+#y1 = []
+#x1_real = []
+#y1_real = []
+#for line in f1.readlines():
+    #l = line.strip().split(',')
+    #x1.append(math.log(int(l[0])))
+    #x1_real.append(int(l[0]))
+    #if int(l[1]) > 0:
+        #y1.append(math.log(int(l[1])))
+        #y1_real.append(int(l[1]))
+    #else:
+        #y1.append(0.0)
+        #y1_real.append(0)
+#x2 = []
+#y2 = []
+#x2_real = []
+#y2_real = []
+#for line in f2.readlines():
+    #l = line.strip().split(',')
+    #x2.append(math.log(int(l[0])))
+    #x2_real.append(int(l[0]))
+    #if int(l[1]) > 0:
+        #y2.append(math.log(int(l[1])))
+        #y2_real.append(int(l[1]))
+    #else:
+        #y2.append(0.0)
+        #y2_real.append(0)
 
 #'''Scatter plot out-degree'''
 #plt.scatter(x1, y1, s=1.0, c='r')
@@ -64,5 +64,21 @@ for line in f2.readlines():
 #workbook.close()
 
 
-#14.90, 15.06, 14.94, 15.34, 15.62
-#205988, 206004, 206060, 205060, 205984
+#[14.90, 15.06, 14.94, 15.34, 15.62]
+#[205988, 206004, 206060, 205060, 205984]
+
+# [333.39, 326.85, 15.00, 16.28, 15.26]
+# [3682856, 3916412, 4107916, 4329308, 4398896]
+
+x = [2.505525936990736, 2.4475508632442313, 2.386006701133118, 2.3194422100604686, 2.2491843162669305, 2.1736147116970854]
+y = [2.61, 1.89, 1.79, 1.77, 1.81, 1.48]
+"""Plot for 2.2"""
+plt.axis([2, math.ceil(max(x)), 0, math.ceil(max(y))])
+plt.xlabel('log(memory)')
+plt.ylabel('Avg System Time in seconds')
+plt.plot(x, y, linewidth=1.)
+plt.title("Performance of disk_sort with different memory")
+plt.show()
+
+
+
